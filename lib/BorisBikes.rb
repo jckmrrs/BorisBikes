527f9f6bac77bfb413d8382aa664eb6f84e664d8
Shamarni <Smcfarlane926@gmail.com>
2020-10-12 17:15:33 +0100

    second test complete

--- lib/BorisBikes.rb
+++ lib/BorisBikes.rb
@@ -1,20 +1,10 @@
 class DockingStation
-    @num_of_bikes = 0
-   def initialise
+   def initialize
      @num_of_bikes = 4
    end
-    
-  def print
-      puts @num_of_bikes
-  end
-    
+
    def release_bike(num)
-    @num_of_bikes = 4 - num
+    @num_of_bikes -= num
    end
-   
+
 end
-
-docking_station = DockingStation.new
-docking_station.print
-
-#docking_station.release_bike(1)
