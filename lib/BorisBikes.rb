81e352577bbabbbd440e130fef87f4e3de3767a7
Jack <jack3009@hotmail.co.uk>
2020-10-12 16:28:59 +0100

    Second test started

--- lib/BorisBikes.rb
+++ lib/BorisBikes.rb
@@ -1,5 +1,13 @@
 class DockingStation
+   def initialise
+     @num_of_bikes = 1
+   end
     
+   def release_bike(num)
+    @num_of_bikes = @num_of_bikes - num
+   end
+   
 end
 
 docking_station = DockingStation.new
+docking_station.release_bike(1)
--- spec/BorisBikes_spec.rb
+++ spec/BorisBikes_spec.rb
@@ -5,6 +5,14 @@ require 'BorisBikes'
 #      it "creates a new docking station"
 #      expect (DockingStation.new(0)).to eq docking_station
 #    end
+
 describe "#DockingStation" do
+
+  describe "#release_bike" do
+      docking_station = DockingStation.new
+      it "removed one bike from the docking station" do
+      expect(docking_station.release_bike(1)).to eq 0
+    end
+    end
 end
-end
+
